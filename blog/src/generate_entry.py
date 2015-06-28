d514587eb308e86d9ae4c2a7b2d3315fb5e2ff73
PseudoRamble <pseudoramble@gmail.com>
2015-06-28 18:13:31 -0400

    Added rough published/modified date functionality

--- blog/src/generate_entry.py
+++ blog/src/generate_entry.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 from pathlib import Path
+from datetime import date
 
 import sys
+import soup_utils
 
 def initialize_view(entry_markup, template_markup):
     temp_view = BeautifulSoup(entry_markup)
@@ -16,7 +18,11 @@ def initialize_view(entry_markup, template_markup):
 
     return view
 
-def update_view(view, prev_entries):
+def store_view(view, filename):
+    with open(filename, 'w') as output_fd:
+        output_fd.write(view.prettify())
+
+def add_previous_entries(view, prev_entries):
     for prev_entry in prev_entries:
         with prev_entry.open() as prev_entry_fd:
             prev_soup = BeautifulSoup(prev_entry_fd.read())
@@ -32,22 +38,42 @@ def update_view(view, prev_entries):
 
     return view
 
-def store_view(view, filename):
-    with open(filename, 'w') as output_fd:
-        output_fd.write(view.prettify())
+def change_dates(view, is_new_entry):
+    published_node = view.new_tag('span')
+    modified_node = view.new_tag('span')
+    dates_node = view.new_tag('div')
+    current_date = date.today()
+    
+    if is_new_entry:
+        soup_utils.add_class(modified_node, 'hidden')
+    else:
+        soup_utils.remove_class(modified_node, 'hidden')
+        
+    published_node.string = "Published on %s " % current_date.isoformat()
+    modified_node.string = "Modified on %s " % current_date.isoformat()
+
+    soup_utils.add_class(dates_node, 'dates')
+    dates_node.append(published_node)
+    dates_node.append(modified_node)
+    view.find('article').insert(1, dates_node)
 
 def _get_previous_entries_(output_dir, cur_filename):
     return [entry for entry in Path(output_dir).glob('*.html') if cur_filename.find(entry.name) < 0]
     
 if len(sys.argv) > 3:
     input_filename, output_filename, template_filename = (sys.argv[1], sys.argv[2], sys.argv[3])
-    output_dir = Path(output_filename).parent
+    output_file_path = Path(output_filename)
+    output_dir = output_file_path.parent
     prev_entries = _get_previous_entries_(output_dir, output_filename)[:5]
-    
+    is_new_entry = not output_file_path.exists()
+
     with open(input_filename) as input_fd, open(template_filename) as template_fd:
         entry_markup = input_fd.read()
         template_markup = template_fd.read()
 
     entry_view = initialize_view(entry_markup, template_markup)
-    update_view(entry_view, prev_entries)
+
+    add_previous_entries(entry_view, prev_entries)
+    change_dates(entry_view, is_new_entry)
+
     store_view(entry_view, output_filename)
