5286dbe2c45f54b4311369987f1a1b494eccc92d
PseudoRamble <pseudoramble@gmail.com>
2016-10-31 21:17:56 -0400

    Finding the previous entries This is a means of figuring out what the previous entries were. Basically will maintain a JSON file of entries and when they were created. The filesystem approach was not proving very reliable, so this seems like a more stable method. Includes looking up entries before/after an entry was created. Show those links in the template.

--- blog/post-dev-server/index.js
+++ blog/post-dev-server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const { lookupContent, setupContent } = require('./src/content.js');
+const { before, after } = require('./src/entryHunter.js');
+
+const toLink = entry => ({ 
+  title: entry.name,
+  url: `/blog/entry/${entry.name}.html`
+});
 
 const app = express();
 
@@ -16,8 +22,10 @@ app.get('/post/:name', (req, res) => {
   const content = lookupContent(entryName);
   const contentStored = setupContent(content);
 
+  const previousEntries = before(entryName).map(toLink);
+
   if (contentStored) {
-    res.render('entry');
+    res.render('entry', { previousEntries });
   } else {
     res.status(503).send('Content could not setup properly for rendering to occur.');
   }
---
+++ blog/post-dev-server/src/entryHunter.js
@@ -0,0 +1,27 @@
+const { readdirSync, statSync } = require('fs');
+const _ = require('lodash');
+
+const writtenEntries = require('./entries.json');
+
+const byNewestFirst = (first, second) => new Date(second.created) - new Date(first.created);
+const byOldestFirst = (first, second) => new Date(first.created) - new Date(second.created);
+
+const summonEntries = ({name = '', comparator = byNewestFirst, limit = 5}) =>
+  _(writtenEntries)
+      .sort(comparator)
+      .dropWhile(entry => name ? entry.name.indexOf(name) < 0 : false)
+      .slice(name && 1 || 0, name && (limit + 1) || limit)
+      .value();
+
+const before = (name, limit=5) => summonEntries({ name });
+const after = (name, limit=5) => summonEntries({ name, comparator: byOldestFirst });
+
+const latest = (name, limit=5) => summonEntries({});
+const oldest = (name, limit=5) => summonEntries({ comparator: byOldestFirst });
+
+module.exports = {
+  before,
+  after,
+  latest,
+  oldest
+}
