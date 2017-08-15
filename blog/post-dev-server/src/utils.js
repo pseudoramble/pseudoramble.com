9fe0116c2d4bbafbb5d60b4cc9d58ffb091665e3
PseudoRamble <pseudoramble@gmail.com>
2017-08-14 21:34:23 -0400

    bug: fixed typo in redirects map

--- blog/post-dev-server/src/utils.js
+++ blog/post-dev-server/src/utils.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
-module.exports = function(filename) {
-  fs.writeFileSync('../data/redirects.map', `/blog /blog/entry/${filename}.html`, { encoding: 'utf-8' });
+module.exports = {
+  updateRedirectsMap: function(filename) {
+    fs.writeFileSync(__dirname + '/../data/redirects.map', `/blog /blog/entry/${filename}.html;`, { encoding: 'utf-8' });
+  }
 };
